292b8c13d6b8f2b09c82ff02c2cffa0243023471
rhryhoryk <eh480.35@gmail.com>
2019-08-19 15:59:33 +0300

    pickup rotation added

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,7 +1,11 @@
-let playButton = document.querySelector('.play');
-let vinyl = document.querySelector('.vinyl');
+let playButton  = document.querySelector('.play');
+let vinyl       = document.querySelector('.vinyl');
+let pickup      = document.querySelector('.pickup')
+
+
 playButton.addEventListener('click', function () {
-    // vinyl.classList.toggle('--vinylaised')
-    if (vinyl.classList.contains('--vinylaised')) { vinyl.classList.remove('--vinylaised')}
-    else {vinyl.classList.add('--vinylaised');}
+  // vinyl.classList.toggle('--vinylaised')
+  if (vinyl.classList.contains('--vinylaised')) { vinyl.classList.remove('--vinylaised')}
+  else {vinyl.classList.add('--vinylaised');}
+  pickup.classList.add('--pickupaised')
  })
