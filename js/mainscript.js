a62fddc52002173c74681d8de2f6f338b2b228b9
rhryhoryk <eh480.35@gmail.com>
2019-08-20 14:08:38 +0300

    pickup-up-down button added

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,11 +1,29 @@
+// buttons
 let playButton  = document.querySelector('.play');
+let updownButton= document.querySelector('.pickup-up-down');
+// objects
 let vinyl       = document.querySelector('.vinyl');
 let pickup      = document.querySelector('.pickup')
 
 
+
+updownButton.addEventListener('click', function () {
+
+  if (pickup.classList.contains('--unpickupaised')) { 
+    pickup.classList.remove('--unpickupaised')
+  }
+
+  if (pickup.classList.contains('--pickupaised')) {
+    pickup.classList.remove('--pickupaised');
+    pickup.classList.add('--unpickupaised');
+  } else {pickup.classList.add('--pickupaised');}
+})
+
 playButton.addEventListener('click', function () {
-  // vinyl.classList.toggle('--vinylaised')
-  if (vinyl.classList.contains('--vinylaised')) { vinyl.classList.remove('--vinylaised')}
-  else {vinyl.classList.add('--vinylaised');}
-  pickup.classList.add('--pickupaised')
+  vinyl.classList.toggle('--vinylaised')
+  
+  if (vinyl.classList.contains('--vinylaised')) {
+    pickup.classList.remove('--unpickupaised');
+    pickup.classList.add('--pickupaised');
+  }
  })
